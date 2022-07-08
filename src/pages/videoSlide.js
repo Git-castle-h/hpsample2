ca104e35fbbbc835047a3dadd5ef8b354a489634
Hyeok <ssp02245@gmail.com>
2022-07-09 02:27:08 +0900

    second-commit

--- src/pages/videoSlide.js
+++ src/pages/videoSlide.js
@@ -9,76 +9,8 @@ import 'swiper/css/parallax';
 
 function VideoSlide(){
     useEffect(function(){
-        let slideArea = document.querySelector('.slideArea');
-        let slideArr = document.querySelectorAll('.slideArea .slide');
-        let drag = false;
-        let originX;
-        let moveX;
-        let curSelect;
-        window.addEventListener('load',slideDup());
-        window.addEventListener('resize',slideDup());
-        function slideDup(){
-            let SAWidth = slideArea.clientWidth;
-            let slideWidth = slideArr[0].clientWidth;
-            let max = true;
-            // console.log( SAWidth>slideWidth*(slideArr.length));
-            for(let i=0; max; i++){
-            if(SAWidth < slideWidth*(slideArr.length)){
-                max = true;
-                // console.log(max);
-            }else{
-                max =false;
-                // console.log(max);
-            }
-            slideArea.appendChild(slideArr[i].cloneNode(true));
-            console.log(slideArr[i]);
-            }
-        }
-        slideArea.addEventListener('mousedown',slideDown);
-        slideArea.addEventListener('mousemove',slideMove);
-        slideArea.addEventListener('mouseup',slideUp);
-        slideArea.addEventListener('mouseleave',slideUp);
-        function slideDown(evt){
-            evt.preventDefault();
-            drag = true;
-            originX = evt.clientX;
-            curSelect = evt.target;
-        }
-        function slideMove(evt){
-            if(drag){
-            let slideWidth = slideArr[0].clientWidth;
-                console.log('move');
-                moveX = evt.clientX;
-                let num =  moveX - originX;
-                slideArea.style.left = num+'px';
-                curSelect.style.pointerEvents ='none';
-                if(num>slideWidth){
-                    slidePrev();
-                }
-                if(num<-slideWidth){
-                    slideNext();
-                }
 
 
-            }
-        }
-        function slideUp(evt){
-            drag = false;
-            curSelect.style.pointerEvents ='auto';
-        }
-        function slidePrev(){
-            let slideP = slideArr[0];
-            slideP.remove();
-            slideArea.appendChild(slideP.cloneNode(true));
-            slideArr = document.querySelectorAll('.slideArea .slide');
-        }
-        function slideNext(){
-            console.log(slideArr.length);
-            let slideN = slideArr[slideArr.length-1];
-            slideN.remove();
-            slideArea.appendChild(slideN.cloneNode(true));
-            slideArr = document.querySelectorAll('.slideArea .slide');
-        }
 
     },[])
     return(
@@ -99,30 +31,55 @@ function VideoSlide(){
                 <button className='subBtn' data-id="interview">면접전형</button>
             </div>
             <div className="slideWrap">
-                <div className="slideArea">
-                    <div className="slide">
+                <div className="slideArea pc">
+                <Swiper
+                    slidesPerView={perView}
+                    spaceBetween={0}
+                    modules={[Pagination]}
+                    className="mySwiper"
+                    loop={true}
+                >
+                        <SwiperSlide>
                         <a href=""><img src={process.env.PUBLIC_URL+'/images/video/thumbnail/video_thumbnail_jobfind01.png'} alt="" /></a>
-                    </div>                    
-                    <div className="slide">
+                        </SwiperSlide>
+                        <SwiperSlide>
                         <a href=""><img src={process.env.PUBLIC_URL+'/images/video/thumbnail/video_thumbnail_jobfind02.png'} alt="" /></a>
-                    </div>
-                    <div className="slide">
+                        </SwiperSlide>
+                        <SwiperSlide>
                         <a href=""><img src={process.env.PUBLIC_URL+'/images/video/thumbnail/video_thumbnail_jobfind03.png'} alt="" /></a>
-                    </div>
-                    <div className="slide">
-                        <a href=""><img src={process.env.PUBLIC_URL+'/images/video/thumbnail/video_thumbnail_jobfind04.png'} alt="" /></a>
-                    </div>
-                    <div className="slide">
+                        </SwiperSlide>
+                        <SwiperSlide>
                         <a href=""><img src={process.env.PUBLIC_URL+'/images/video/thumbnail/video_thumbnail_jobfind05.png'} alt="" /></a>
-                    </div>
-                    <div className="slide">
+                        </SwiperSlide>
+                        <SwiperSlide>
                         <a href=""><img src={process.env.PUBLIC_URL+'/images/video/thumbnail/video_thumbnail_jobfind06.png'} alt="" /></a>
-                    </div>
+                        </SwiperSlide>
+                </Swiper>
                 </div>
 
             </div>
         </section>
     )
 }
+
+let perView;
+window.addEventListener('load',setPerview);
+window.addEventListener('resize',setPerview);
+function setPerview(){
+    let windowWidth = window.innerWidth;
+    console.log(windowWidth);
+    if(windowWidth>1500){
+        perView = 7;
+    }else if(windowWidth>1023){
+        perView = 5;
+    }else if(windowWidth>768){
+        perView=3;
+    }else if(windowWidth>500){
+        perView= 2;
+    } else{
+        perView= 1;
+    }
+}
+
 
 export{VideoSlide}
