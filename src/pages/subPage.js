82243e45fb239e91ffaecb52627c67a03c15c0d5
ESH <ssp02245@gmail.com>
2022-08-02 18:34:51 +0900

    subPage-commit

--- src/pages/subPage.js
+++ src/pages/subPage.js
@@ -44,40 +44,44 @@ function Sub(){
                 </div>
             </section>
             <section className='subName sub'>
-                <div className="titleArea">
-                    <div className="mainTitleWrap">
-                        <h1 className="mainTitle">서브페이지2 타이틀</h1>
+                <div className="inWrap">
+                    <div className="titleArea">
+                        <div className="mainTitleWrap">
+                            <h1 className="mainTitle">서브페이지2 타이틀</h1>
+                        </div>
+                        <div className="subTitleWrap">
+                            <h2 className="subTitle">서브페이지2 서브타이틀</h2>
+                        </div>
                     </div>
-                    <div className="subTitleWrap">
-                        <h2 className="subTitle">서브페이지2 서브타이틀</h2>
-                    </div>
-                </div>
-                <div className="contextArea">
-                    <div className="imageWrap">
-                    </div>
-                    <div className="textWrap">
-                        <p>
-                        마디씩 것은 노새, 한 있습니다. 나는 속의 했던 멀리 이름을 헤는 써 나의 까닭입니다. 하나에 아침이 둘 멀리 까닭입니다. 노새, 어머님, 가을 겨울이 하나에 사랑과 프랑시스 계십니다. 아침이 덮어 별 오면 한 봅니다. 다 별들을 새겨지는 책상을 없이 내 듯합니다. 별 벌레는 나의 거외다. 어머니, 어머니 이름과, 덮어 속의 이름과, 나는 파란 봅니다. 이 이국 나는 토끼, 사람들의 다 걱정도 하나에 계십니다. 옥 멀리 써 별 이름을 토끼, 위에 경, 별 까닭입니다. 나는 하나에 겨울이 소학교 사람들의 하나에 못 덮어 아무 듯합니다.
-                        </p>
+                    <div className="contextArea">
+                        <div className="imageWrap">
+                        </div>
+                        <div className="textWrap">
+                            <p>
+                            마디씩 것은 노새, 한 있습니다. 나는 속의 했던 멀리 이름을 헤는 써 나의 까닭입니다. 하나에 아침이 둘 멀리 까닭입니다. 노새, 어머님, 가을 겨울이 하나에 사랑과 프랑시스 계십니다. 아침이 덮어 별 오면 한 봅니다. 다 별들을 새겨지는 책상을 없이 내 듯합니다. 별 벌레는 나의 거외다. 어머니, 어머니 이름과, 덮어 속의 이름과, 나는 파란 봅니다. 이 이국 나는 토끼, 사람들의 다 걱정도 하나에 계십니다. 옥 멀리 써 별 이름을 토끼, 위에 경, 별 까닭입니다. 나는 하나에 겨울이 소학교 사람들의 하나에 못 덮어 아무 듯합니다.
+                            </p>
+                        </div>
                     </div>
                 </div>
             </section>
             <section className='subName sub'>
-                <div className="titleArea">
-                    <div className="mainTitleWrap">
-                        <h1 className="mainTitle">서브페이지3 타이틀</h1>
+                <div className="inWrap">
+                    <div className="titleArea">
+                        <div className="mainTitleWrap">
+                            <h1 className="mainTitle">서브페이지3 타이틀</h1>
+                        </div>
+                        <div className="subTitleWrap">
+                            <h2 className="subTitle">서브페이지3 서브타이틀</h2>
+                        </div>
                     </div>
-                    <div className="subTitleWrap">
-                        <h2 className="subTitle">서브페이지3 서브타이틀</h2>
-                    </div>
-                </div>
-                <div className="contextArea">
-                    <div className="imageWrap">
-                    </div>
-                    <div className="textWrap">
-                        <p>
-                        마디씩 것은 노새, 한 있습니다. 나는 속의 했던 멀리 이름을 헤는 써 나의 까닭입니다. 하나에 아침이 둘 멀리 까닭입니다. 노새, 어머님, 가을 겨울이 하나에 사랑과 프랑시스 계십니다. 아침이 덮어 별 오면 한 봅니다. 다 별들을 새겨지는 책상을 없이 내 듯합니다. 별 벌레는 나의 거외다. 어머니, 어머니 이름과, 덮어 속의 이름과, 나는 파란 봅니다. 이 이국 나는 토끼, 사람들의 다 걱정도 하나에 계십니다. 옥 멀리 써 별 이름을 토끼, 위에 경, 별 까닭입니다. 나는 하나에 겨울이 소학교 사람들의 하나에 못 덮어 아무 듯합니다.
-                        </p>
+                    <div className="contextArea">
+                        <div className="imageWrap">
+                        </div>
+                        <div className="textWrap">
+                            <p>
+                            마디씩 것은 노새, 한 있습니다. 나는 속의 했던 멀리 이름을 헤는 써 나의 까닭입니다. 하나에 아침이 둘 멀리 까닭입니다. 노새, 어머님, 가을 겨울이 하나에 사랑과 프랑시스 계십니다. 아침이 덮어 별 오면 한 봅니다. 다 별들을 새겨지는 책상을 없이 내 듯합니다. 별 벌레는 나의 거외다. 어머니, 어머니 이름과, 덮어 속의 이름과, 나는 파란 봅니다. 이 이국 나는 토끼, 사람들의 다 걱정도 하나에 계십니다. 옥 멀리 써 별 이름을 토끼, 위에 경, 별 까닭입니다. 나는 하나에 겨울이 소학교 사람들의 하나에 못 덮어 아무 듯합니다.
+                            </p>
+                        </div>
                     </div>
                 </div>
             </section>
